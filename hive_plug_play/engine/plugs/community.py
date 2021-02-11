f3b798f3bc1645f87e03dd5d9178b4781e530027
imwatsi <imwatsi@gmail.com>
2021-02-11 13:23:52 +0200

    add community unsubscribe op

--- hive_plug_play/engine/plugs/community.py
+++ hive_plug_play/engine/plugs/community.py
@@ -1,5 +1,3 @@
-
-from hive_plug_play.database.handlers import PlugPlayDb
 from hive_plug_play.server.system_status import SystemStatus
 
 class SearchOps:
@@ -7,7 +5,7 @@ class SearchOps:
     @classmethod
     def subscribe(cls, community, block_range=None):
         """
-        "subscribe"    | {"community":"hive-178179"}
+            "subscribe"    | {"community":"hive-178179"}
         """
         if block_range is None:
             latest = SystemStatus.get_latest_block()
@@ -22,5 +20,26 @@ class SearchOps:
                     AND (op_json -> 0)::text = '"subscribe"'
                     AND block_num BETWEEN {block_range[0]} and {block_range[1]} 
             )AS subscribe_ops;
+        """
+        return query
+    
+    @classmethod
+    def unsubscribe(cls, community, block_range=None):
+        """
+            :unsubscribe"  | {"community":"hive-152200"}
+        """
+        if block_range is None:
+            latest = SystemStatus.get_latest_block()
+            if not latest: return None # TODO: notify??
+            block_range = [latest - 28800, latest]
+        query = f"""
+            SELECT *  
+            FROM (
+                SELECT req_posting_auths [1]
+                FROM custom_json_ops
+                WHERE op_id = 'community'
+                    AND (op_json -> 0)::text = '"unsubscribe"'
+                    AND block_num BETWEEN {block_range[0]} and {block_range[1]} 
+            )AS unsubscribe_ops;
         """
         return query
