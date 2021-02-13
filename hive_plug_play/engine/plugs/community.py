7ce028bcee8924fb68bb26dbaff41e5650bce065
imwatsi <imwatsi@gmail.com>
2021-02-13 12:10:32 +0200

    flag post op

--- hive_plug_play/engine/plugs/community.py
+++ hive_plug_play/engine/plugs/community.py
@@ -43,3 +43,41 @@ class SearchOps:
             )AS unsubscribe_ops;
         """
         return query
+    
+    @classmethod
+    def flag_post(cls, community=None, account=None, permlink=None, notes=None, block_range=None):
+        """Populates query to retrieve `flagPost` ops"""
+        if block_range is None:
+            latest = SystemStatus.get_latest_block()
+            if not latest: return None # TODO: notify??
+            block_range = [latest - 28800, latest]
+        query = f"""
+            SELECT *  
+            FROM (
+                SELECT req_posting_auths [1],
+                    op_json -> 1 -> 'account'::text,
+                    op_json -> 1 -> 'permlink'::text,
+                    op_json -> 1 -> 'notes'::text
+                FROM custom_json_ops
+                WHERE op_id = 'community'
+                    AND (op_json -> 0)::text = '"flagPost"'
+                    AND block_num BETWEEN {block_range[0]} and {block_range[1]} 
+        """
+        if community:
+            query += f"""
+            AND (op_json -> 1 -> 'community'):: text = '"{community}"'
+            """
+        if account:
+            query += f"""
+            AND (op_json -> 1 -> 'account'):: text = '"{account}"'
+            """
+        if permlink:
+            query += f"""
+            AND (op_json -> 1 -> 'permlink'):: text = '"{permlink}"'
+            """
+        if notes:
+            query += f"""
+            AND (op_json -> 1 -> 'notes'):: text = '"{notes}"'
+            """
+        query += ")AS flag_post_ops;"
+        return query
--- hive_plug_play/server/plug_endpoints/community.py
+++ hive_plug_play/server/plug_endpoints/community.py
@@ -10,7 +10,7 @@ async def get_subscribe_ops(context, community, block_range=None):
     sql = SearchOps.subscribe(community, block_range=block_range)
     result = []
     if sql:
-        res = db.db.select(sql)
+        res = db.db.select(sql)  or []
         for entry in res:
             result.append(populate_by_schema(
                 entry, ['account']
@@ -18,14 +18,27 @@ async def get_subscribe_ops(context, community, block_range=None):
     return result
 
 async def get_unsubscribe_ops(context, community, block_range=None):
-    """Returns a list of community unsubscribe ops within the speicified block range."""
+    """Returns a list of community unsubscribe ops within the specified block range."""
     db = context['db']
     sql = SearchOps.unsubscribe(community, block_range=block_range)
     result = []
     if sql:
-        res = db.db.select(sql)
+        res = db.db.select(sql) or []
         for entry in res:
             result.append(populate_by_schema(
                 entry, ['account']
+            ))
+    return result
+
+async def get_flag_post_ops(context, community=None, account=None, permlink=None, notes=None, block_range=None):
+    """Returns a list of `flagPost` ops within the speicified block range."""
+    db = context['db']
+    sql = SearchOps.flag_post(community, account, permlink, notes, block_range)
+    result = []
+    if sql:
+        res = db.db.select(sql) or []
+        for entry in res:
+            result.append(populate_by_schema(
+                entry, ['acc_auths', 'account', 'permlink', 'notes']
             ))
     return result
