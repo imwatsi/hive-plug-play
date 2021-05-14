a889819b5f781075bf1b918528648c46ec15210a
imwatsi <imwatsi@gmail.com>
2021-05-14 13:07:44 +0200

    improved hive requests

--- hive_plug_play/hive/server_requests.py
+++ hive_plug_play/hive/server_requests.py
@@ -10,8 +10,11 @@ class NodeLoads:
 
     def __init__(self):
         self.counts = {}
+        self.error_counts = {}
         self.reset_counts()
         self.minute = datetime.utcnow().minute
+        for n in HIVE_NODES:
+            self.error_counts[n] = 0
     
     def reset_counts(self):
         self.counts.clear()
@@ -25,12 +28,19 @@ class NodeLoads:
             self.minute = cur_minute
         self.counts[node] += 1
     
+    def record_error(self, node):
+        self.error_counts[node] += 1
+    
+    def record_ok(self, node):
+        self.error_counts[node] = 0
+    
     def get_node_to_use(self):
         lowest = None
         for node in self.counts:
-            cur_count = self.counts[node]
-            if not lowest: lowest = [node, cur_count]
-            if lowest[1] > cur_count: lowest = [node, cur_count]
+            if self.error_counts[node] < 3:
+                cur_count = self.counts[node]
+                if not lowest: lowest = [node, cur_count]
+                if lowest[1] > cur_count: lowest = [node, cur_count]
         return lowest[0]
         
 hive_nodes = NodeLoads()
@@ -49,11 +59,13 @@ def make_request(method, params=[]):
             hive_nodes.record_req(node)
             req = requests.post(node, data=json.dumps(payload), timeout=3)
             result = json.loads(req.content)['result']
+            hive_nodes.record_ok(node)
             return result
         except Exception as e:
             # TODO: log error
+            hive_nodes.record_error(node)
             if count == 20:
                 # TODO: error, safe shutdown, log
                 print(e)
                 os._exit(1)
-            time.sleep(0.5)
+            time.sleep(0.1)
